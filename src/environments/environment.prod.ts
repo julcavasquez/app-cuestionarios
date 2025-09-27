365b80ea532e0706f077180c6b0d9afd9867d735
Julca Vasquez <jvandersson93@gmail.com>
2025-09-27 01:50:28 -0500

    update variable de produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api-cuestionarios-production.up.railway.app/' // aquí inyectaremos desde Vercel
+  apiUrl: 'https://api-cuestionarios-production.up.railway.app' // aquí inyectaremos desde Vercel
 };
